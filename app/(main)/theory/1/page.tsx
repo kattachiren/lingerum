a325b14b6d92c0c708010aecea08d18a3047af66
kattachiren <103179040+kattachiren@users.noreply.github.com>
2024-06-20 22:01:08 +0300

    frfr

--- app/(main)/theory/1/page.tsx
+++ app/(main)/theory/1/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import { redirect } from "next/navigation";
 
 import { Promo } from "@/components/promo";
--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -5,7 +5,6 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-  auth,
 } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
